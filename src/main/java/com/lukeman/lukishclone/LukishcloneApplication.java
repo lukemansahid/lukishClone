fa8a7bbd1fab4ae705a01ea44fa67b59e433d56d
Lukeman Kamara <lukemansahid2020@gmail.com>
2020-11-21 00:21:01 +0100

    initial commit

---
+++ src/main/java/com/lukeman/lukishclone/LukishcloneApplication.java
@@ -0,0 +1,13 @@
+package com.lukeman.lukishclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukishcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LukishcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukeman/lukishclone/LukishcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukeman.lukishclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukishcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
